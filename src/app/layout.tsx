9c40670171efc9d2d014a13d6f108bdaf0481295
Sam Gutentag <1404219+samgutentag@users.noreply.github.com>
2025-08-04 23:43:53 -0700

    add vercel speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
